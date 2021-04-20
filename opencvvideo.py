236930a54ff8755dd83727c66ab5e6ed89c92f6f
RidhamDiyora <81667641+RidhamDiyora@users.noreply.github.com>
2021-04-20 06:48:37 -0700

    Delete opencvvideo.py

--- opencvvideo.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture('C:/Users/Ridham Diyora/.vscode/python projects/pictures/panda video.webm')
-
-while True:
-    success , img = cap.read()
-    cv2.imshow('video', img)
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
